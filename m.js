f8ed361c4e7c1983452c3d7a14fb27e5d2077e59
mearg <meargtakelle@gmail.com>
2024-08-26 12:58:42 +0300

    js

---
+++ m.js
@@ -0,0 +1,11 @@
+// const x= 5;
+// const y= 2*x+50;
+// console.log(y);
+// const c=12;
+// const f=c*1.8+32;
+// console.log(f);
+const x ='a';
+console.log(x.charCodeAt(0));
+
+// const m ='M'
+// console.log(m.charCodeAt(1));
